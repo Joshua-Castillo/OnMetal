d713cd37f9689f1b63c6090b160069187bdb960f
Joshua Castillo <joshcastillo2048@gmail.com>
2024-06-17 12:27:40 -0400

    Initial Commit

---
+++ OnMetal/OnMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnMetalApp.swift
+//  OnMetal
+//
+//  Created by Joshua Castillo on 2024-06-17.
+//
+
+import SwiftUI
+
+@main
+struct OnMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
