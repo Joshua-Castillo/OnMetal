149f3fdc4961eb00d09ad24f5026caec8bb67c52
Joshua Castillo <joshcastillo2048@gmail.com>
2024-06-17 17:22:22 -0400

    initial commit, render pipeline

---
+++ OnMetal/Shaders.metal
@@ -0,0 +1,11 @@
+//
+//  Shaders.metal
+//  OnMetal
+//
+//  Created by Joshua Castillo on 2024-06-17.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
---
+++ OnMetal/definitions.h
@@ -0,0 +1,12 @@
+//
+//  definitions.h
+//  OnMetal
+//
+//  Created by Joshua Castillo on 2024-06-17.
+//
+
+#ifndef definitions_h
+#define definitions_h
+
+
+#endif /* definitions_h */
